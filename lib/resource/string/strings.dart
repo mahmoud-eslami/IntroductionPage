bd174c9ad27035a6ddae1f1dda87e05585b4e5e6
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-04-07 00:34:00 +0430

    App String Added

--- lib/resource/string/strings.dart
+++ lib/resource/string/strings.dart
@@ -1,3 +1,9 @@
-class String {
-
+class Strings {
+  static const String skipBtn = 'Skip';
+  static const String firstTitle =
+      'Browse the menu and order directly from the application';
+  static const String secondTitle =
+      'Your order will be immediately collected sent by our courier';
+  static const String thirdTitle =
+      'Pick up delivery at your door and enjoy groceries';
 }
