6ac62ed61db1fe0344bd317fd18bcf21461702e9
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-04-07 00:12:46 +0430

    Initial commit

---
+++ lib/screen/first_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatefulWidget {
+  @override
+  _FirstPageState createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screen/third_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ThirdPage extends StatefulWidget {
+  @override
+  _ThirdPageState createState() => _ThirdPageState();
+}
+
+class _ThirdPageState extends State<ThirdPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
